883e7bb7cf6dd5b3b21240ba110d6581d015dabf
Elsherif, Marwan <marwanelsheriif@gmail.com>
2024-11-12 18:39:21 +0200

    first commit

---
+++ src/components/photovoltaic_panel.py
@@ -0,0 +1,13 @@
+
+
+
+class PVPanel():
+    def __init__(self, power, voltage, current) -> None:
+        self.power = power
+        self.voltage = voltage
+        self.current = current
+
+
+    
+
+         
---
+++ src/components/storage_system/inverter.py
@@ -0,0 +1,7 @@
+
+
+class Inverter():
+    def __init__(self, max_power, voltage, current) -> None:
+        self.max_power = max_power
+        self.voltage = voltage
+        self.current = current
